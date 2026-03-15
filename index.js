55dcac130c09a89dc8a2ac438e27cb2b18640299
mahamadmhamad63-oss <mahamadmhamad63@gmail.com>
2026-03-15 14:10:54 +0200

    Improve bot message handling and responses
    Refactor message handling and update bot responses.

--- index.js
+++ index.js
@@ -1,42 +1,24 @@
 const { default: makeWASocket, useMultiFileAuthState } = require("@whiskeysockets/baileys")
+const P = require("pino")
 
 async function startBot() {
-
 const { state, saveCreds } = await useMultiFileAuthState("session")
 
 const sock = makeWASocket({
-auth: state,
-printQRInTerminal: true
+logger: P({ level: "silent" }),
+auth: state
 })
 
 sock.ev.on("creds.update", saveCreds)
 
 sock.ev.on("messages.upsert", async ({ messages }) => {
-
 const msg = messages[0]
 if (!msg.message) return
 
 const text = msg.message.conversation || msg.message.extendedTextMessage?.text
-const from = msg.key.remoteJid
 
 if (text === "اوامر") {
-
-await sock.sendMessage(from,{
-text:
-`🤖 هينا بوت
-
-اوامر الادمن
-فتح
-قفل
-طرد
-خفض
-منشن
-
-اوامر المطور
-طرد_متعدد
-اخرج`
-})
-
+await sock.sendMessage(msg.key.remoteJid, { text: "اهلا بك في هينا بوت 🤖" })
 }
 
 })
